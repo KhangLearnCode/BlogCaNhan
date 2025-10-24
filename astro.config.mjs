baf224dd461d4e7b520e6fad7215cb04c90d7f42
Lê Nguyễn Duy Khang <lekhang1617@gmail.com>
2025-10-25 00:40:51 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Có thể thay bằng domain Pages khi đã triển khai: https://khandlearncode.github.io/blog-ca-nhan-khang/
-  site: 'https://khandlearncode.github.io/',
+  site: 'https://khanglearncode.github.io/BlogCaNhan',
+  base: '/BlogCaNhan',
 });
