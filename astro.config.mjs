fc3835878f78fc485337a46930caf023f22a1550
Lê Nguyễn Duy Khang <lekhang1617@gmail.com>
2025-10-25 02:09:14 +0700

    Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://khanglearncode.github.io/BlogCaNhan',
   base: '/BlogCaNhan',
+  integrations: [sitemap()],
 });
